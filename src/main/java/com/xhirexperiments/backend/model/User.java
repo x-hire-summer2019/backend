a99cd31c6ef7f7198a30aabdc0d9d6cc0b287672
mike muponda <mupondamike@gmail.com>
2019-09-04 15:10:01 -0500

    User Class complete

--- src/main/java/com/xhirexperiments/backend/model/User.java
+++ src/main/java/com/xhirexperiments/backend/model/User.java
@@ -1,4 +1,39 @@
 package com.xhirexperiments.backend.model;
-
 public class User {
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String role;
+
+}
+public User(long id, String firstname, String lastname,String email, String role){
+    this.id=id;
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.email=email;
+    this.role=role;
+}
+
+public User(){
+
+}
+public long getId(){
+    return id;
+}
+
+public String getFirstName(){
+    return firstname;
+}
+public String getLastName(){
+    return lastname;
+}
+public String getEmail(){
+    return email;
+}
+
+public String getRole(){
+    return role;
+}
+
 }
