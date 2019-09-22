34d3deff7fb08219eef041a532708c5cbd7e78da
Ashlynne Garcia <agarcia@xpanxion.com>
2019-09-22 15:43:53 -0500

    updated db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,22 @@
 insert into Pod(id, pod_Name, description, organizer, organizer_Email, created, status )
-values(100, 'Test Name', 'Testing description', 'Ashlynne', 'ash@gmail.com', sysdate(), true);
+values(1, 'AI/ML & Data Science', 'Focus on competency building on AI/ML and Python.
+Competency building on Azure ML, AWS ML and Google TensorFlow', 'Unknown', 'unknown', sysdate(), true);
 
 insert into Pod(id, pod_Name, description, organizer, organizer_Email, created, status )
-values(101, 'Test Name 2', 'Decription Test', 'Mike', 'mike@gmail.com', sysdate(), true);
+values(2, 'Java', 'Focus on building micro services based architectures using Spring Boot and Spring Cloud.
+Evaluation & competency building of upcoming server less architectures', 'Unknown', 'Unknown', sysdate(), true);
+
+insert into Pod(id, pod_Name, description, organizer, organizer_Email, created, status )
+values(3, 'Blockchain', 'Initial competency building on Blockchain concepts. Competency building on Azure Blockchain, Hyperledger fabric & Corda.', 'Unknown', 'Unknown', sysdate(), true);
+
+insert into Pod(id, pod_Name, description, organizer, organizer_Email, created, status )
+values(4, 'Microsoft', 'Focus on building micro services starter kit using Azure, Docker & Kubernetes. Evaluation of Azure IOT,  Stream Analytics & Cognitive Services capabilities.', 'Unknown', 'Unknown', sysdate(), true);
+
+insert into Pod(id, pod_Name, description, organizer, organizer_Email, created, status )
+values(5, 'Mobility', 'Focus on building Xamarin forms starter kit. Competency building in PWAs, React Native and Ionic based mobility frameworks. ', 'Unknown', 'Unknown', sysdate(), true);
+
+insert into Pod(id, pod_Name, description, organizer, organizer_Email, created, status )
+values(5, 'JS Frameworks', 'Focus on Angular Starter Kit upgrade. Competency building on ReactJS, NodeJS and VueJS. ', 'Unknown', 'Unknown', sysdate(), true);
+
+insert into Pod(id, pod_Name, description, organizer, organizer_Email, created, status )
+values(5, 'DevOps(QA + Tech)', 'Focus on containers and container orchestration tools like Docker, Kubernetes. Competency building on cloud orchestration and configuration tools like Ansible. ', 'Unknown', 'Unknown', sysdate(), true);
